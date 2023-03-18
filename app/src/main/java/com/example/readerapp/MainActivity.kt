951b0d799b5b0f2e90f40a4c3644236c61d09ba6
aderemilekun <adelefatai@yahoo.com>
2023-03-18 17:23:31 +0100

    Hilt base setup

--- app/src/main/java/com/example/readerapp/MainActivity.kt
+++ app/src/main/java/com/example/readerapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.readerapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,17 +12,33 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.readerapp.ui.theme.ReaderAppTheme
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ReaderAppTheme {
+
+                val db = FirebaseFirestore.getInstance()
+                val user: MutableMap<String, Any> = HashMap()
+                user["firstName"] = "James"
+                user["lastName"] = "Jill"
+                
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    db.collection("users").add(user).addOnSuccessListener {
+                        Log.d("FirstDataSuccess", "onCreate: ${it.id}")
+                    }.addOnFailureListener {
+                        Log.d("FirstDataFail", "onCreate: ${it}")
+                    }
+
                     Greeting("Android")
                 }
             }
