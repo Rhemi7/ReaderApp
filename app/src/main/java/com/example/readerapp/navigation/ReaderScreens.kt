421b1e3a64b2edc5854cefe5f05e37e8c2bbf07d
aderemilekun <adelefatai@yahoo.com>
2023-03-19 22:59:23 +0100

    Navigation enums

--- app/src/main/java/com/example/readerapp/navigation/ReaderScreens.kt
+++ app/src/main/java/com/example/readerapp/navigation/ReaderScreens.kt
@@ -1,5 +1,29 @@
 package com.example.readerapp.navigation
 
 enum class ReaderScreens {
+    SplashScreen,
+    LoginScreen,
+    CreateAccountScreen,
+    ReaderHomeScreen,
+    SearchScreen,
+    DetailsScreen,
+    UpdateScreen,
+    ReaderStatsScreen;
 
+
+    companion object {
+        fun fromRoute(route: String?): ReaderScreens
+        = when(route?.substringBefore("/")) {
+            SplashScreen.name -> SplashScreen
+            LoginScreen.name -> LoginScreen
+            CreateAccountScreen.name -> CreateAccountScreen
+            ReaderStatsScreen.name -> ReaderStatsScreen
+            SearchScreen.name -> SearchScreen
+            DetailsScreen.name -> DetailsScreen
+            UpdateScreen.name -> UpdateScreen
+            ReaderHomeScreen.name -> ReaderHomeScreen
+            null -> ReaderHomeScreen
+            else -> throw IllegalArgumentException("Route $route is not recognized")
+        }
+    }
 }
