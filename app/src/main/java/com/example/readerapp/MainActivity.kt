4cad12a7b11270bbbad99f0f6f5c9e547c64e0a1
aderemilekun <adelefatai@yahoo.com>
2023-03-21 10:45:51 +0100

    Configured Navigation

--- app/src/main/java/com/example/readerapp/MainActivity.kt
+++ app/src/main/java/com/example/readerapp/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.readerapp.navigation.ReaderNavigation
 import com.example.readerapp.ui.theme.ReaderAppTheme
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,39 +28,42 @@ class MainActivity : ComponentActivity() {
         setContent {
             ReaderAppTheme {
 
-                val db = FirebaseFirestore.getInstance()
-                val user: MutableMap<String, Any> = HashMap()
-                user["firstName"] = "James"
-                user["lastName"] = "Jill"
+//                val db = FirebaseFirestore.getInstance()
+//                val user: MutableMap<String, Any> = HashMap()
+//                user["firstName"] = "James"
+//                user["lastName"] = "Jill"
                 
 
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    db.collection("users").add(user).addOnSuccessListener {
-                        Log.d("FirstDataSuccess", "onCreate: ${it.id}")
-                    }.addOnFailureListener {
-                        Log.d("FirstDataFail", "onCreate: ${it}")
-                    }
 
-                    Greeting("Android")
-                }
+//                    db.collection("users").add(user).addOnSuccessListener {
+//                        Log.d("FirstDataSuccess", "onCreate: ${it.id}")
+//                    }.addOnFailureListener {
+//                        Log.d("FirstDataFail", "onCreate: ${it}")
+//                    }
+
+                ReaderApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ReaderApp() {
+  Surface(color = MaterialTheme.colors.background,
+      modifier = Modifier.fillMaxSize()) {
+      Column(
+          verticalArrangement = Arrangement.Center,
+          horizontalAlignment = Alignment.CenterHorizontally
+      ) {
+          ReaderNavigation()
+      }
+  }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ReaderAppTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/readerapp/screens/SplashScreen.kt
+++ app/src/main/java/com/example/readerapp/screens/SplashScreen.kt
@@ -1,10 +1,11 @@
 package com.example.readerapp.screens
 
 import android.window.SplashScreen
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
 fun SplashScreen(navController: NavController) {
-
+    Text("SplashScreen")
 }
