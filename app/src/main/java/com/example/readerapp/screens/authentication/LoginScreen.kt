ec8b4b6eb699e49325515d5c4979f46b0864617c
aderemilekun <adelefatai@yahoo.com>
2023-03-21 14:58:36 +0100

    Created user form for password and email

--- app/src/main/java/com/example/readerapp/screens/authentication/LoginScreen.kt
+++ app/src/main/java/com/example/readerapp/screens/authentication/LoginScreen.kt
@@ -1,15 +1,15 @@
 package com.example.readerapp.screens.authentication
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -19,14 +19,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.readerapp.components.EmailInput
 import com.example.readerapp.components.InputField
+import com.example.readerapp.components.PasswordInput
 import com.example.readerapp.components.ReaderText
 
 @Composable
@@ -37,13 +42,22 @@ fun LoginScreen(navController: NavController) {
             verticalArrangement = Arrangement.Top
         ) {
             ReaderText()
+            UserForm(loading = false, isCreateAccount = false) {
+                email, password ->
+                Log.d("Form", "LoginScreen: $email $password")
+            }
         }
     }
 }
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun UserForm() {
+fun UserForm(
+    loading: Boolean = false,
+    isCreateAccount: Boolean = false,
+    onDone: (String, String) -> Unit = {email, pwd ->}
+
+) {
     val email = rememberSaveable {
         mutableStateOf("")
     }
@@ -53,7 +67,7 @@ fun UserForm() {
     }
     
     val passwordVisibility = rememberSaveable {
-        mutableStateOf("")
+        mutableStateOf(false)
     }
 
     val passwordFocusRequest = FocusRequester.Default
@@ -72,30 +86,30 @@ fun UserForm() {
         )
 
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        EmailInput(emailState = email, enabled = true, onAction = KeyboardActions {
+        EmailInput(
+            emailState = email,
+            enabled = !loading,
+            onAction = KeyboardActions {
             passwordFocusRequest.requestFocus()
         })
+
+        PasswordInput(
+            modifier = Modifier.focusRequester(passwordFocusRequest),
+            passwordState = password,
+            labelId = "Password",
+            enabled = !loading,
+            passwordVisibility = passwordVisibility,
+            onAction = KeyboardActions {
+
+                if (!valid) return@KeyboardActions
+                onDone(email.value.trim(), password.value.trim())
+            }) {
+
+        }
     }
 }
 
-@Composable
-fun EmailInput(
-    modifier: Modifier = Modifier,
-    emailState: MutableState<String>,
-    labelId: String = "Email",
-    enabled: Boolean = true,
-    imeAction: ImeAction = ImeAction.Next,
-    onAction: KeyboardActions = KeyboardActions.Default
-) {
 
-    InputField(
-        modifier = modifier,
-        valueState = emailState,
-        labelId = labelId,
-        enabled = enabled,
-        keyboardType = KeyboardType.Email,
-        imeAction = imeAction,
-        onAction = onAction
-    )
-}
+
+
 
