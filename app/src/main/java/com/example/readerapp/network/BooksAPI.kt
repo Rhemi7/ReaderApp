1cd625fbde019404b5796ed5849a93207934c333
aderemilekun <adelefatai@yahoo.com>
2023-03-23 14:03:46 +0100

    Created Books Api retrofit interface and provider

---
+++ app/src/main/java/com/example/readerapp/network/BooksAPI.kt
@@ -0,0 +1,18 @@
+package com.example.readerapp.network
+
+import com.example.readerapp.model.Book
+import com.example.readerapp.model.Item
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import javax.inject.Singleton
+
+@Singleton
+interface BooksAPI {
+
+    @GET("volumes")
+    suspend fun getAllBooks(@Query(value = "q") query: String): Book
+
+    @GET("volumes/{bookId}")
+    suspend fun getBookInfo(@Path("bookId") bookId: String): Item
+}
