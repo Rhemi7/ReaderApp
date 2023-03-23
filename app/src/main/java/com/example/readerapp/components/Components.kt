264799771c65ae820d3670621bcbdfbac72aba85
aderemilekun <adelefatai@yahoo.com>
2023-03-23 13:29:27 +0100

    Set up search screen structure

--- app/src/main/java/com/example/readerapp/components/Components.kt
+++ app/src/main/java/com/example/readerapp/components/Components.kt
@@ -268,7 +268,7 @@ fun BookListCard(
         Column(modifier = Modifier.width(screenWidth.dp - (spacing * 2)), horizontalAlignment = Alignment.Start) {
             Row(horizontalArrangement = Arrangement.Center) {
                 Image(
-                    painter = rememberAsyncImagePainter(""),
+                    painter = rememberAsyncImagePainter("https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=80&q=80"),
                     contentDescription = "Book Image",
                     modifier = Modifier
                         .height(140.dp)
--- app/src/main/java/com/example/readerapp/screens/search/SearchScreen.kt
+++ app/src/main/java/com/example/readerapp/screens/search/SearchScreen.kt
@@ -1,27 +1,32 @@
 package com.example.readerapp.screens.search
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
 import com.example.readerapp.components.InputField
 import com.example.readerapp.components.ReaderAppBar
+import com.example.readerapp.model.MBook
 import com.example.readerapp.navigation.ReaderScreens
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -40,12 +45,66 @@ fun SearchScreen(navController: NavController = NavController(LocalContext.curre
         }
     }) {
         Surface() {
-            Column() {
+            val listOfBooks = listOf<MBook>(
+                MBook(id = "gddh", title = "Things fall apart", authors = "All of us", notes = null),
+                MBook(id = "gddh", title = "Things fall apart", authors = "All of us", notes = null),
+                MBook(id = "gddh", title = "Things fall apart", authors = "All of us", notes = null),
+                MBook(id = "gddh", title = "Things fall apart", authors = "All of us", notes = null),
+                MBook(id = "gddh", title = "Things fall apart", authors = "All of us", notes = null)
+            )
+
+            Column {
                 SearchForm(modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp) )
+                Spacer(modifier = Modifier.height(13.dp))
+
+
+
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    contentPadding = PaddingValues(16.dp)
+                ) {
+                    items(listOfBooks) { book ->
+                        BookRowCard(book = book, navController = navController)
+                    }
+                }
             }
         }
+    }
+}
+
+@Composable
+fun BookRowCard(book: MBook, navController: NavController) {
+    Card(modifier = Modifier
+        .padding(3.dp)
+        .fillMaxWidth()
+        .height(100.dp)
+        .clickable { },
+        elevation = 7.dp
+    ) {
+        Row(verticalAlignment = Alignment.Top) {
+            val imageUrl = "https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=80&q=80"
+            Image(
+                painter = rememberAsyncImagePainter(imageUrl),
+                contentDescription = "Book Image",
+                modifier = Modifier
+                    .width(80.dp)
+                    .fillMaxHeight()
+                    .padding(
+                        end = 4.dp
+                    )
+            )
+            Column() {
+                Text(text = book.title.toString(), overflow = TextOverflow.Ellipsis)
+                Text(
+                    text = "Authors ${book.authors}",
+                    overflow = TextOverflow.Clip,
+                    style = MaterialTheme.typography.caption
+                )
+            }
+        }
+
     }
 }
 
