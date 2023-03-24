c8e80d82e9435ccd1e7bbbd76e45c594c983ba44
aderemilekun <adelefatai@yahoo.com>
2023-03-24 17:21:22 +0100

    Saving books to firebase implemented

--- app/src/main/java/com/example/readerapp/screens/details/ReaderBookDetailsScreen.kt
+++ app/src/main/java/com/example/readerapp/screens/details/ReaderBookDetailsScreen.kt
@@ -1,18 +1,18 @@
+@file:Suppress("NAME_SHADOWING")
+
 package com.example.readerapp.screens.details
 
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Center
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -21,7 +21,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.text.HtmlCompat
@@ -34,8 +33,8 @@ import com.example.readerapp.data.Resource
 import com.example.readerapp.model.Item
 import com.example.readerapp.model.MBook
 import com.example.readerapp.navigation.ReaderScreens
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.ktx.Firebase
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -85,20 +84,23 @@ fun ReaderBookDetailsScreen(navController: NavController,
                         fontWeight = FontWeight.Bold,
                         fontSize = 20.sp, textAlign = TextAlign.Center
                     )
-                    val bookInfo = bookInfo.data.volumeInfo
-                    Text(text = "Authors: ${bookInfo.authors}")
-                    Text(text = "Page Count: ${bookInfo.pageCount}")
+                    val bookData = bookInfo.data.volumeInfo
+                    val googleBookId = bookInfo.data.id
+
+
+                    Text(text = "Authors: ${bookData.authors}")
+                    Text(text = "Page Count: ${bookData.pageCount}")
                     Text(
-                        text = "Categories: ${bookInfo.categories}",
+                        text = "Categories: ${bookData.categories}",
                         overflow = TextOverflow.Ellipsis,
                         maxLines = 3
                     )
                     Text(
-                        text = "Published: ${bookInfo.publishedDate}",
+                        text = "Published: ${bookData.publishedDate}",
                         textAlign = TextAlign.Center
                     )
                     val cleanDesc = HtmlCompat.fromHtml(
-                        bookInfo.description,
+                        bookData.description,
                         HtmlCompat.FROM_HTML_MODE_LEGACY
                     ).toString()
 
@@ -139,8 +141,19 @@ fun ReaderBookDetailsScreen(navController: NavController,
                             "Save",
                             radius = 30
                         ) {
-                            val book = MBook()
-                            saveToFirebase(book)
+                            val book = MBook(
+                                title = bookData.title.toString(),
+                                authors = bookData.authors.toString(),
+                                description = bookData.description,
+                                categories = bookData.categories.toString(),
+                                notes = "",
+                                photoUrl = bookData.imageLinks.thumbnail,
+                                publishedDate = bookData.publishedDate,
+                                rating = 0.0,
+                                googleBookId = googleBookId,
+                                userId = FirebaseAuth.getInstance().currentUser?.uid
+                            )
+                            saveToFirebase(book, navController)
                         }
                         Spacer(modifier = Modifier.width(20.dp))
                         RoundedButton(
@@ -160,6 +173,23 @@ fun ReaderBookDetailsScreen(navController: NavController,
 
 }
 
-fun saveToFirebase(book: MBook) {
+fun saveToFirebase(book: MBook, navController: NavController) {
     val db = FirebaseFirestore.getInstance()
+    val dbCollection = db.collection("books")
+
+    if (book.toString().isNotEmpty()) {
+        dbCollection.add(book).addOnSuccessListener {
+            documentRef ->
+            val docId = documentRef.id
+            dbCollection.document(docId).update(hashMapOf("id" to docId) as Map<String, Any>).addOnCompleteListener{ task ->
+                if (task.isSuccessful) {
+                    navController.popBackStack()
+                }
+
+            }.addOnFailureListener {
+                Log.d("Doc Update Error", "saveToFirebase: Error Updating Doc")
+
+            }
+        }
+    }
 }
